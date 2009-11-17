d8b7bd8f4960a70df0f267a3cb0cfe144d407691
shane <shane@62d5b8a3-27da-0310-9561-8e5933582275>
2009-11-17 23:30:23 +0000

    Buildout now depends on a released version of RelStorage.  prep 0.2.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """A ZODB performance test"""
 
-version = "0.1"
+version = "0.2"
 
 from setuptools import setup, find_packages
 import os
