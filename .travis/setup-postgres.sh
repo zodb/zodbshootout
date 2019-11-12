7497b73d522ea02254c417e51c4427c067f97b6f
Jason Madden <jamadden@gmail.com>
2019-11-12 06:47:36 -0600

    Debugging why psycopg2cffi isnt available.

--- .travis/setup-postgres.sh
+++ .travis/setup-postgres.sh
@@ -1,4 +1,6 @@
 pip install -U -e ".[postgresql]"
+pip install -U pg8000 psycopg2cffi
+python -c 'import psycopg2cffi'
 psql -U postgres -c "CREATE USER relstoragetest WITH PASSWORD 'relstoragetest';"
 psql -U postgres -c "CREATE DATABASE relstoragetest OWNER relstoragetest;"
 psql -U postgres -c "CREATE DATABASE relstoragetest2 OWNER relstoragetest;"
